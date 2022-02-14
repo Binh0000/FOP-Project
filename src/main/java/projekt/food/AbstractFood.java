4596b4d8161f5dbdc2703730140e54797fede109
Binh0000 <phianbinh2001@gmail.com>
2022-02-14 17:17:09 +0100

    Update AbstractFood.java

--- src/main/java/projekt/food/AbstractFood.java
+++ src/main/java/projekt/food/AbstractFood.java
@@ -4,11 +4,13 @@ import java.math.BigDecimal;
 import java.util.List;
 import java.util.function.DoubleUnaryOperator;
 import java.util.function.UnaryOperator;
+import java.util.stream.Collector;
 
 public class AbstractFood implements Food{
 	private BigDecimal price;
 	private double weight;
 	private Variant<?, ?> foodVariant;
+	private List<? extends Extra<?>> extras;
 	
 	@Override
 	public BigDecimal getPrice() {
@@ -27,24 +29,35 @@ public class AbstractFood implements Food{
 
 	@Override
 	public List<? extends Extra<?>> getExtras() {
-		return null;
+		return extras;
 	}
 	
 	private static class Config implements Food.Config {
+		private List<UnaryOperator<BigDecimal>> priceMutators;
+		private List<DoubleUnaryOperator> weightMutators;
 
 		@Override
 		public void price(UnaryOperator<BigDecimal> priceMutator) {
-			
+			priceMutators.add(priceMutator);
 		}
 
 		@Override
 		public UnaryOperator<BigDecimal> getPriceMutator() {
-			return null;
+			UnaryOperator<BigDecimal> composition = priceMutators.stream().collect(null);
+			UnaryOperator<BigDecimal> temp = null;
+			for(UnaryOperator<BigDecimal> priceMutator: priceMutators) {
+				if(temp == null) {
+					temp = priceMutator;
+				} else {
+					temp = (n -> priceMutator.apply(n));
+				}
+			}
+			return composition;
 		}
 
 		@Override
 		public void weight(DoubleUnaryOperator weightMutator) {
-			
+			weightMutators.add(weightMutator);
 		}
 
 		@Override
