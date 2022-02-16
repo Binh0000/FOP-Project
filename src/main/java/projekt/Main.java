d978f0ca9a447ab90303f4f9d3780f1380ccfe9d
Krazmer <dudeahn26@gmail.com>
2022-02-16 13:06:10 +0100

    AbstractFood

--- src/main/java/projekt/Main.java
+++ src/main/java/projekt/Main.java
@@ -3,6 +3,7 @@ package projekt;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!"); 
+        System.out.println("Hello World!");
+        System.out.println("Chad");
     }
 }
