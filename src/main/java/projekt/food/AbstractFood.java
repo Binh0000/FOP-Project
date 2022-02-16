4658e937f2570936bfa5bbc9ef474b71714cb5b5
Krazmer <dudeahn26@gmail.com>
2022-02-16 12:57:54 +0100

    AbstractFood

--- src/main/java/projekt/food/AbstractFood.java
+++ src/main/java/projekt/food/AbstractFood.java
@@ -5,29 +5,29 @@ import java.util.List;
 import java.util.function.DoubleUnaryOperator;
 import java.util.function.UnaryOperator;
 
-class AbstractFood implements Food {
+abstract class AbstractFood implements Food {
 	final BigDecimal price;
 	final double weight;
 	final Food.Variant<? extends Food, ? extends Food.Config> foodVariant;
 	final List<? extends Extra<?>> extras;
-	
+
 	/**
 	 * Constructs an object that is an implementation of {@link Food}
-	 * 
+	 *
 	 * @param price price of this food
 	 * @param weight weight of this food
 	 * @param foodVariant variant of this food
 	 * @param extras extras compatible with this food
 	 */
-	public AbstractFood(BigDecimal price, double weight, 
-			Food.Variant<? extends Food, ? extends Food.Config> foodVariant, 
+	public AbstractFood(BigDecimal price, double weight,
+			Food.Variant<? extends Food, ? extends Food.Config> foodVariant,
 			List<? extends Extra<?>> extras) {
 		this.price = price;
 		this.weight = weight;
 		this.foodVariant = foodVariant;
 		this.extras = extras;
 	}
-	
+
 	@Override
     /**
      * The price of this food.
@@ -37,7 +37,7 @@ class AbstractFood implements Food {
 	public BigDecimal getPrice() {
 		return price;
 	}
-	
+
 	@Override
     /**
      * The weight of this food.
@@ -67,19 +67,19 @@ class AbstractFood implements Food {
 	public List<? extends Extra<?>> getExtras() {
 		return extras;
 	}
-	
+
 	/**
-	 * 
-	 * 
+	 *
+	 *
 	 *
 	 */
 	static class Config implements Food.Config {
 		BigDecimal p;
 		double w;
-		
+
 		List<UnaryOperator<BigDecimal>> priceMutators;
 		List<DoubleUnaryOperator> weightMutators;
-		
+
 		Config(BigDecimal p, double w) {
 			this.p = p;
 			this.w = w;
@@ -87,7 +87,7 @@ class AbstractFood implements Food {
 
 		@Override
 		/**
-		 * 
+		 *
 		 */
 		public void price(UnaryOperator<BigDecimal> priceMutator) {
 			p = priceMutator.apply(p);
@@ -96,17 +96,17 @@ class AbstractFood implements Food {
 
 		@Override
 		/**
-		 * 
+		 *
 		 */
-		public UnaryOperator<BigDecimal> getPriceMutator() {			
-			return priceMutators.stream()														  
+		public UnaryOperator<BigDecimal> getPriceMutator() {
+			return priceMutators.stream()
 					 			 .reduce((n -> n),
 					 					 (op1, op2) -> (UnaryOperator<BigDecimal>) op1.compose(op2));
 		}
 
 		@Override
 		/**
-		 * 
+		 *
 		 */
 		public void weight(DoubleUnaryOperator weightMutator) {
 			w = weightMutator.applyAsDouble(w);
@@ -115,24 +115,24 @@ class AbstractFood implements Food {
 
 		@Override
 		/**
-		 * 
+		 *
 		 */
 		public DoubleUnaryOperator getWeightMutator() {
-			return weightMutators.stream()														  
+			return weightMutators.stream()
 					 			 .reduce((n -> n),
 					 					 (op1, op2) -> op1.compose(op2));
-		}		
+		}
 	}
-	
+
 	static class Variant<F extends Food, C extends Food.Config> implements Food.Variant<F, C> {
-		
+
 		String name;
 		FoodType<F, C> foodType;
 		BigDecimal basePrice;
 		double baseWeight;
-		
+
 		/**
-		 * 
+		 *
 		 * @param name
 		 * @param foodType
 		 * @param basePrice
@@ -171,10 +171,10 @@ class AbstractFood implements Food {
 		}
 
 		@Override
-		public F create(List<? extends Extra<? super C>> extras) {			
+		public F create(List<? extends Extra<? super C>> extras) {
 			return null;
 		}
 
-		
+
 	}
 }
